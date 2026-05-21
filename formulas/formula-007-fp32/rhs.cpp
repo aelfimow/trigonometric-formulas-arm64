5f267a4e5370d7ac9209e656e097f8db9bca496c
Alexander Elfimow <my.free.mail@gmx.de>
2026-05-21 21:06:56 +0200

    formulas/formula-007-fp32: added files

---
+++ formulas/formula-007-fp32/rhs.cpp
@@ -0,0 +1,11 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <cmath>
+
+fp32 rhs(fp32 alpha, fp32 beta)
+{
+    fp32 const result = (sinf(alpha) * cosf(beta)) - (cosf(alpha) * sinf(beta));
+
+    return result;
+}
