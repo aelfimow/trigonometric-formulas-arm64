b280530fc4f6c162b206ca89f6346738437ba342
Alexander Elfimow <my.free.mail@gmx.de>
2026-05-25 19:52:23 +0200

    formulas/formula-008-fp80: added files

---
+++ formulas/formula-008-fp80/macheps.h
@@ -0,0 +1,8 @@
+#ifdef macheps_h
+#error Already included.
+#else
+#define macheps_h
+
+extern fp80 macheps();
+
+#endif
---
+++ formulas/formula-008-fp80/rhs.cpp
@@ -0,0 +1,11 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+fp80 rhs(fp80 alpha, fp80 beta)
+{
+    fp80 const result = (cosl(alpha) * cosl(beta)) - (sinl(alpha) * sinl(beta));
+
+    return result;
+}
