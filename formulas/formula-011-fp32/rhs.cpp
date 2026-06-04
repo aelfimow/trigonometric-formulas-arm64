f822044699d10f0c107516323f59ef936242fdb6
Alexander Elfimow <my.free.mail@gmx.de>
2026-06-04 20:15:21 +0200

    formulas/formula-011-fp32: added files

---
+++ formulas/formula-011-fp32/rhs.cpp
@@ -0,0 +1,14 @@
+#include "fp32.h"
+#include "rhs.h"
+
+#include <cmath>
+
+fp32 rhs(fp32 alpha, fp32 beta)
+{
+    fp32 const a = tanf(alpha);
+    fp32 const b = tanf(beta);
+
+    fp32 const result = (a - b) / (1.0f + (a * b));
+
+    return result;
+}
