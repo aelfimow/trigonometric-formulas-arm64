b3f48dc29bcf69bcd9ac5aa6d3a0388b517dd207
Alexander Elfimow <my.free.mail@gmx.de>
2026-06-04 20:15:40 +0200

    formulas/formula-011-fp64: added files

---
+++ formulas/formula-011-fp64/rhs.cpp
@@ -0,0 +1,14 @@
+#include "fp64.h"
+#include "rhs.h"
+
+#include <cmath>
+
+fp64 rhs(fp64 alpha, fp64 beta)
+{
+    fp64 const a = tan(alpha);
+    fp64 const b = tan(beta);
+
+    fp64 const result = (a - b) / (1.0 + (a * b));
+
+    return result;
+}
