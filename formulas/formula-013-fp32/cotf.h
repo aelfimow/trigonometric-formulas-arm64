44aa60271ea153b9b0b56759056b348de793fcd2
Alexander Elfimow <my.free.mail@gmx.de>
2026-07-13 19:13:37 +0200

    formulas/formula-013-fp32: added

---
+++ formulas/formula-013-fp32/cotf.h
@@ -0,0 +1,8 @@
+#ifdef cotf_h
+#error Already included.
+#else
+#define cotf_h
+
+extern fp32 cotf(fp32 x);
+
+#endif
---
+++ formulas/formula-013-fp32/lhs.cpp
@@ -0,0 +1,10 @@
+#include "fp32.h"
+#include "lhs.h"
+#include "cotf.h"
+
+fp32 lhs(fp32 alpha, fp32 beta)
+{
+    fp32 const result = cotf(alpha - beta);
+
+    return result;
+}
