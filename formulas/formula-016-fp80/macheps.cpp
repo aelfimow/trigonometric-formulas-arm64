d9a58848ee9d2173146943989e87c329902c0c68
Alexander Elfimow <my.free.mail@gmx.de>
2026-08-01 21:38:02 +0200

    formula/formula-016-fp80: added

---
+++ formulas/formula-016-fp80/macheps.cpp
@@ -0,0 +1,7 @@
+#include "fp80.h"
+#include "macheps.h"
+
+fp80 macheps()
+{
+    return 5.42101e-20L;
+}
---
+++ formulas/formula-016-fp80/rhs.cpp
@@ -0,0 +1,20 @@
+#include "fp80.h"
+#include "rhs.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \sqrt{1 - \cos^{2}{\alpha}}
+ * @f]
+ */
+fp80 rhs(fp80 alpha)
+{
+    fp80 const a = cosl(alpha);
+
+    fp80 const result = sqrtl(1.0L - (a * a));
+
+    return result;
+}
