65456f6a08f04db6c0cc4e8ea5c3c1657b1578ad
Alexander Elfimow <my.free.mail@gmx.de>
2026-08-02 12:15:30 +0200

    formula-018-fp64: added

---
+++ formulas/formula-018-fp64/rhs.cpp
@@ -0,0 +1,21 @@
+#include "fp64.h"
+#include "rhs.h"
+#include "cot.h"
+
+#include <cmath>
+
+/**
+ * Right Hand Side:
+ *
+ * @f[
+ *     \frac{1}{\sqrt{1 + \cot^{2}{\alpha}}}
+ * @f]
+ */
+fp64 rhs(fp64 alpha)
+{
+    fp64 const a = cot(alpha);
+
+    fp64 const result = 1.0 / sqrt(1.0 + (a * a));
+
+    return result;
+}
